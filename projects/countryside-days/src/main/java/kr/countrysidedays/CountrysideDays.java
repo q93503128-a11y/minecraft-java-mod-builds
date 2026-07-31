e28f70fda642f00935c4528c61d68d4c97c77dbc
coworkaccount <cyp1jhh2@gmail.com>
2026-07-31 15:00:01 +0900

    Add Countryside Days mod entrypoint

---
+++ projects/countryside-days/src/main/java/kr/countrysidedays/CountrysideDays.java
@@ -0,0 +1,27 @@
+package kr.countrysidedays;
+
+import com.mojang.logging.LogUtils;
+import kr.countrysidedays.registry.ModBlocks;
+import kr.countrysidedays.registry.ModCreativeTabs;
+import kr.countrysidedays.registry.ModItems;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import org.slf4j.Logger;
+
+@Mod(CountrysideDays.MOD_ID)
+public final class CountrysideDays {
+    public static final String MOD_ID = "countrysidedays";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public CountrysideDays(IEventBus modEventBus) {
+        ModBlocks.register(modEventBus);
+        ModItems.register(modEventBus);
+        ModCreativeTabs.register(modEventBus);
+        modEventBus.addListener(this::commonSetup);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+        LOGGER.info("Countryside Days {} core content registered", "0.1.0-alpha.2");
+    }
+}
