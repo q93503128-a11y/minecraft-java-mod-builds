5a3ad556fffed15dd839efdf46a00128e358279d
coworkaccount <cyp1jhh2@gmail.com>
2026-07-31 15:23:48 +0900

    Register Countryside Days kitchen interactions

--- projects/countryside-days/src/main/java/kr/countrysidedays/CountrysideDays.java
+++ projects/countryside-days/src/main/java/kr/countrysidedays/CountrysideDays.java
@@ -1,12 +1,14 @@
 package kr.countrysidedays;
 
 import com.mojang.logging.LogUtils;
+import kr.countrysidedays.gameplay.KitchenInteractionHandler;
 import kr.countrysidedays.registry.ModBlocks;
 import kr.countrysidedays.registry.ModCreativeTabs;
 import kr.countrysidedays.registry.ModItems;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
 import org.slf4j.Logger;
 
 @Mod(CountrysideDays.MOD_ID)
@@ -19,6 +21,7 @@ public final class CountrysideDays {
         ModItems.register(modEventBus);
         ModCreativeTabs.register(modEventBus);
         modEventBus.addListener(this::commonSetup);
+        NeoForge.EVENT_BUS.addListener(KitchenInteractionHandler::onUseItemOnBlock);
     }
 
     private void commonSetup(FMLCommonSetupEvent event) {
