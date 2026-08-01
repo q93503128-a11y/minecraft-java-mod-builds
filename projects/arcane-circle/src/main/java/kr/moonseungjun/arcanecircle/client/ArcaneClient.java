a56a3176cc6cd01a5565a5d20b090db80c02f603
coworkaccount <cyp1jhh2@gmail.com>
2026-08-01 12:15:09 +0900

    Add separate focus weave and fusion controls

--- projects/arcane-circle/src/main/java/kr/moonseungjun/arcanecircle/client/ArcaneClient.java
+++ projects/arcane-circle/src/main/java/kr/moonseungjun/arcanecircle/client/ArcaneClient.java
@@ -6,7 +6,6 @@ import kr.moonseungjun.arcanecircle.network.RequestGrimoirePayload;
 import kr.moonseungjun.arcanecircle.network.SelectSlotPayload;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
-import net.minecraft.network.chat.Component;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 import net.neoforged.neoforge.client.network.ClientPacketDistributor;
@@ -16,35 +15,38 @@ public final class ArcaneClient {
             "key.arcanecircle.grimoire", InputConstants.KEY_C, KeyMapping.Category.MISC);
     private static final KeyMapping CAST_KEY = new KeyMapping(
             "key.arcanecircle.cast", InputConstants.KEY_R, KeyMapping.Category.MISC);
-    private static final KeyMapping PREVIOUS_KEY = new KeyMapping(
-            "key.arcanecircle.previous_spell", InputConstants.KEY_Z, KeyMapping.Category.MISC);
-    private static final KeyMapping NEXT_KEY = new KeyMapping(
-            "key.arcanecircle.next_spell", InputConstants.KEY_X, KeyMapping.Category.MISC);
+    private static final KeyMapping FUSION_KEY = new KeyMapping(
+            "key.arcanecircle.fusion_cast", InputConstants.KEY_G, KeyMapping.Category.MISC);
+    private static final KeyMapping PREVIOUS_FOCUS_KEY = new KeyMapping(
+            "key.arcanecircle.previous_focus", InputConstants.KEY_Z, KeyMapping.Category.MISC);
+    private static final KeyMapping NEXT_FOCUS_KEY = new KeyMapping(
+            "key.arcanecircle.next_focus", InputConstants.KEY_X, KeyMapping.Category.MISC);
+    private static final KeyMapping PREVIOUS_WEAVE_KEY = new KeyMapping(
+            "key.arcanecircle.previous_weave", InputConstants.KEY_V, KeyMapping.Category.MISC);
+    private static final KeyMapping NEXT_WEAVE_KEY = new KeyMapping(
+            "key.arcanecircle.next_weave", InputConstants.KEY_B, KeyMapping.Category.MISC);
 
     private ArcaneClient() {}
 
     public static void registerKeys(RegisterKeyMappingsEvent event) {
         event.register(GRIMOIRE_KEY);
         event.register(CAST_KEY);
-        event.register(PREVIOUS_KEY);
-        event.register(NEXT_KEY);
+        event.register(FUSION_KEY);
+        event.register(PREVIOUS_FOCUS_KEY);
+        event.register(NEXT_FOCUS_KEY);
+        event.register(PREVIOUS_WEAVE_KEY);
+        event.register(NEXT_WEAVE_KEY);
     }
 
     public static void onClientTick(ClientTickEvent.Pre event) {
         Minecraft minecraft = Minecraft.getInstance();
         if (minecraft.player == null) return;
-        while (GRIMOIRE_KEY.consumeClick()) ClientPacketDistributor.sendToServer(new RequestGrimoirePayload("spells"));
-        while (CAST_KEY.consumeClick()) ClientPacketDistributor.sendToServer(new CastSpellPayload(ArcaneClientState.selected()));
-        while (PREVIOUS_KEY.consumeClick()) select(minecraft, -1);
-        while (NEXT_KEY.consumeClick()) select(minecraft, 1);
-    }
-
-    private static void select(Minecraft minecraft, int delta) {
-        int selected = Math.floorMod(ArcaneClientState.selected() + delta, 5);
-        ClientPacketDistributor.sendToServer(new SelectSlotPayload(selected));
-        String spellId = ArcaneClientState.slots().get(selected);
-        String name = kr.moonseungjun.arcanecircle.magic.SpellCatalog.spell(spellId)
-                .map(kr.moonseungjun.arcanecircle.magic.SpellDefinition::name).orElse("빈 슬롯");
-        minecraft.player.sendOverlayMessage(Component.literal("§5[" + (selected + 1) + "] §f" + name));
+        while (GRIMOIRE_KEY.consumeClick()) ClientPacketDistributor.sendToServer(new RequestGrimoirePayload("atlas"));
+        while (CAST_KEY.consumeClick()) ClientPacketDistributor.sendToServer(new CastSpellPayload(0));
+        while (FUSION_KEY.consumeClick()) ClientPacketDistributor.sendToServer(new CastSpellPayload(1));
+        while (PREVIOUS_FOCUS_KEY.consumeClick()) ClientPacketDistributor.sendToServer(new SelectSlotPayload(0));
+        while (NEXT_FOCUS_KEY.consumeClick()) ClientPacketDistributor.sendToServer(new SelectSlotPayload(1));
+        while (PREVIOUS_WEAVE_KEY.consumeClick()) ClientPacketDistributor.sendToServer(new SelectSlotPayload(2));
+        while (NEXT_WEAVE_KEY.consumeClick()) ClientPacketDistributor.sendToServer(new SelectSlotPayload(3));
     }
 }
