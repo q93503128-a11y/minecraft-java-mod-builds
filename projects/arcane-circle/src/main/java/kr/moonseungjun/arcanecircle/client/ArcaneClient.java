8362fcfa0e8565a6eacf65ea5142887f268b11e9
coworkaccount <cyp1jhh2@gmail.com>
2026-08-01 17:34:57 +0900

    Replace Arcane controls with 1-5 slots and hold-X fusion

--- projects/arcane-circle/src/main/java/kr/moonseungjun/arcanecircle/client/ArcaneClient.java
+++ projects/arcane-circle/src/main/java/kr/moonseungjun/arcanecircle/client/ArcaneClient.java
@@ -2,8 +2,9 @@ package kr.moonseungjun.arcanecircle.client;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import kr.moonseungjun.arcanecircle.network.CastSpellPayload;
+import kr.moonseungjun.arcanecircle.network.CommitFusionPayload;
+import kr.moonseungjun.arcanecircle.network.QueueFusionPayload;
 import kr.moonseungjun.arcanecircle.network.RequestGrimoirePayload;
-import kr.moonseungjun.arcanecircle.network.SelectSlotPayload;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
@@ -13,40 +14,47 @@ import net.neoforged.neoforge.client.network.ClientPacketDistributor;
 public final class ArcaneClient {
     private static final KeyMapping GRIMOIRE_KEY = new KeyMapping(
             "key.arcanecircle.grimoire", InputConstants.KEY_C, KeyMapping.Category.MISC);
-    private static final KeyMapping CAST_KEY = new KeyMapping(
-            "key.arcanecircle.cast", InputConstants.KEY_R, KeyMapping.Category.MISC);
-    private static final KeyMapping FUSION_KEY = new KeyMapping(
-            "key.arcanecircle.fusion_cast", InputConstants.KEY_G, KeyMapping.Category.MISC);
-    private static final KeyMapping PREVIOUS_FOCUS_KEY = new KeyMapping(
-            "key.arcanecircle.previous_focus", InputConstants.KEY_Z, KeyMapping.Category.MISC);
-    private static final KeyMapping NEXT_FOCUS_KEY = new KeyMapping(
-            "key.arcanecircle.next_focus", InputConstants.KEY_X, KeyMapping.Category.MISC);
-    private static final KeyMapping PREVIOUS_WEAVE_KEY = new KeyMapping(
-            "key.arcanecircle.previous_weave", InputConstants.KEY_V, KeyMapping.Category.MISC);
-    private static final KeyMapping NEXT_WEAVE_KEY = new KeyMapping(
-            "key.arcanecircle.next_weave", InputConstants.KEY_B, KeyMapping.Category.MISC);
+    private static final KeyMapping FUSION_MODIFIER_KEY = new KeyMapping(
+            "key.arcanecircle.fusion_modifier", InputConstants.KEY_X, KeyMapping.Category.MISC);
+    private static final KeyMapping[] SLOT_KEYS = {
+            new KeyMapping("key.arcanecircle.slot_1", InputConstants.KEY_1, KeyMapping.Category.MISC),
+            new KeyMapping("key.arcanecircle.slot_2", InputConstants.KEY_2, KeyMapping.Category.MISC),
+            new KeyMapping("key.arcanecircle.slot_3", InputConstants.KEY_3, KeyMapping.Category.MISC),
+            new KeyMapping("key.arcanecircle.slot_4", InputConstants.KEY_4, KeyMapping.Category.MISC),
+            new KeyMapping("key.arcanecircle.slot_5", InputConstants.KEY_5, KeyMapping.Category.MISC)
+    };
+    private static boolean fusionWasDown;
 
     private ArcaneClient() {}
 
     public static void registerKeys(RegisterKeyMappingsEvent event) {
         event.register(GRIMOIRE_KEY);
-        event.register(CAST_KEY);
-        event.register(FUSION_KEY);
-        event.register(PREVIOUS_FOCUS_KEY);
-        event.register(NEXT_FOCUS_KEY);
-        event.register(PREVIOUS_WEAVE_KEY);
-        event.register(NEXT_WEAVE_KEY);
+        event.register(FUSION_MODIFIER_KEY);
+        for (KeyMapping key : SLOT_KEYS) event.register(key);
     }
 
     public static void onClientTick(ClientTickEvent.Pre event) {
         Minecraft minecraft = Minecraft.getInstance();
-        if (minecraft.player == null) return;
-        while (GRIMOIRE_KEY.consumeClick()) ClientPacketDistributor.sendToServer(new RequestGrimoirePayload("atlas"));
-        while (CAST_KEY.consumeClick()) ClientPacketDistributor.sendToServer(new CastSpellPayload(0));
-        while (FUSION_KEY.consumeClick()) ClientPacketDistributor.sendToServer(new CastSpellPayload(1));
-        while (PREVIOUS_FOCUS_KEY.consumeClick()) ClientPacketDistributor.sendToServer(new SelectSlotPayload(0));
-        while (NEXT_FOCUS_KEY.consumeClick()) ClientPacketDistributor.sendToServer(new SelectSlotPayload(1));
-        while (PREVIOUS_WEAVE_KEY.consumeClick()) ClientPacketDistributor.sendToServer(new SelectSlotPayload(2));
-        while (NEXT_WEAVE_KEY.consumeClick()) ClientPacketDistributor.sendToServer(new SelectSlotPayload(3));
+        if (minecraft.player == null) {
+            fusionWasDown = false;
+            return;
+        }
+
+        while (GRIMOIRE_KEY.consumeClick()) {
+            ClientPacketDistributor.sendToServer(new RequestGrimoirePayload("atlas"));
+        }
+        if (minecraft.screen != null) return;
+
+        boolean fusionDown = FUSION_MODIFIER_KEY.isDown();
+        for (int slot = 0; slot < SLOT_KEYS.length; slot++) {
+            while (SLOT_KEYS[slot].consumeClick()) {
+                if (fusionDown) ClientPacketDistributor.sendToServer(new QueueFusionPayload(slot));
+                else ClientPacketDistributor.sendToServer(new CastSpellPayload(slot));
+            }
+        }
+        if (fusionWasDown && !fusionDown) {
+            ClientPacketDistributor.sendToServer(new CommitFusionPayload(0));
+        }
+        fusionWasDown = fusionDown;
     }
 }
