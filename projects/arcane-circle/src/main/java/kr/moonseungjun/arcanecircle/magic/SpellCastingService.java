6a8c7c25137b2f4e99f7780cfe15b68105689bdc
coworkaccount <cyp1jhh2@gmail.com>
2026-08-01 18:48:18 +0900

    Use Minecraft 26.2 tamable entity package

--- projects/arcane-circle/src/main/java/kr/moonseungjun/arcanecircle/magic/SpellCastingService.java
+++ projects/arcane-circle/src/main/java/kr/moonseungjun/arcanecircle/magic/SpellCastingService.java
@@ -12,7 +12,7 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.animal.TamableAnimal;
+import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
