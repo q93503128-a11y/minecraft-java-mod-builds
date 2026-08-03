1d95cde6e22bb1b15aa66ced732b262d24a47e56
coworkaccount <cyp1jhh2@gmail.com>
2026-08-03 10:31:59 +0900

    Stage Village Guardians v0.17.14 custom effect entities

---
+++ projects/village-guardians/tools/v01714/VillageSkillEffectEntities.java
@@ -0,0 +1,30 @@
+package kr.moonseungjun.villageguardians;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public final class VillageSkillEffectEntities {
+    private static final DeferredRegister.Entities ENTITIES =
+            DeferredRegister.createEntities(VillageGuardians.MOD_ID);
+
+    public static final DeferredHolder<EntityType<?>, EntityType<VillageSkillEffectEntity>> SKILL_EFFECT =
+            ENTITIES.registerEntityType(
+                    "skill_effect",
+                    VillageSkillEffectEntity::new,
+                    MobCategory.MISC,
+                    builder -> builder
+                            .sized(0.1f, 0.1f)
+                            .noSave()
+                            .noSummon()
+                            .clientTrackingRange(128)
+                            .updateInterval(1));
+
+    private VillageSkillEffectEntities() {}
+
+    public static void register(IEventBus modEventBus) {
+        ENTITIES.register(modEventBus);
+    }
+}
