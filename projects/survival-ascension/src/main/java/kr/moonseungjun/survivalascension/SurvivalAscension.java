1700b2766c8caec529e8ee4506e9dfe89cba2870
coworkaccount <cyp1jhh2@gmail.com>
2026-08-22 22:56:53 +0900

    Register tick-drained Woodcutting for Survival Ascension 0.15 [skip ci]

--- projects/survival-ascension/src/main/java/kr/moonseungjun/survivalascension/SurvivalAscension.java
+++ projects/survival-ascension/src/main/java/kr/moonseungjun/survivalascension/SurvivalAscension.java
@@ -21,7 +21,7 @@ import org.slf4j.Logger;
 @Mod(SurvivalAscension.MOD_ID)
 public final class SurvivalAscension {
     public static final String MOD_ID = "survivalascension";
-    public static final String VERSION = "0.14.0-alpha.1";
+    public static final String VERSION = "0.15.0-alpha.1";
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public SurvivalAscension(IEventBus modEventBus) {
@@ -32,6 +32,7 @@ public final class SurvivalAscension {
         NeoForge.EVENT_BUS.addListener(MiningProgression::onBlockBreak);
         NeoForge.EVENT_BUS.addListener(BoreMiningService::onServerTick);
         NeoForge.EVENT_BUS.addListener(WoodcuttingProgression::onBlockBreak);
+        NeoForge.EVENT_BUS.addListener(WoodcuttingProgression::onServerTick);
         NeoForge.EVENT_BUS.addListener(HarvestingProgression::onBreakSpeed);
         NeoForge.EVENT_BUS.addListener(HarvestingProgression::onBlockBreak);
         NeoForge.EVENT_BUS.addListener(IrrigationReplantService::onServerTick);
@@ -47,6 +48,6 @@ public final class SurvivalAscension {
         NeoForge.EVENT_BUS.addListener(EliteMobSystem::onLivingDeath);
         NeoForge.EVENT_BUS.addListener(AscensionAffixes::onEliteDeath);
         NeoForge.EVENT_BUS.addListener(AscensionCommands::onRegisterCommands);
-        LOGGER.info("Survival Ascension {} loaded: six skills + shared infrastructure + elite gear economy", VERSION);
+        LOGGER.info("Survival Ascension {} loaded: six skills + safe scaled work + shared infrastructure", VERSION);
     }
 }
