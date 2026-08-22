433d65f26f5482c571a34331546ff64f8c1158bf
coworkaccount <cyp1jhh2@gmail.com>
2026-08-23 08:34:41 +0900

    Bump Survival Ascension source version to 0.19 [skip ci]

--- projects/survival-ascension/src/main/java/kr/moonseungjun/survivalascension/SurvivalAscension.java
+++ projects/survival-ascension/src/main/java/kr/moonseungjun/survivalascension/SurvivalAscension.java
@@ -24,7 +24,7 @@ import org.slf4j.Logger;
 @Mod(SurvivalAscension.MOD_ID)
 public final class SurvivalAscension {
     public static final String MOD_ID = "survivalascension";
-    public static final String VERSION = "0.18.0-alpha.1";
+    public static final String VERSION = "0.19.0-alpha.1";
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public SurvivalAscension(IEventBus modEventBus) {
