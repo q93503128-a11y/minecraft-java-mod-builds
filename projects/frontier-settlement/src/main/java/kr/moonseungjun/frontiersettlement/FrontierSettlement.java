f59a32fb44fb3cb9a70ec58c35e901622a6c346a
coworkaccount <cyp1jhh2@gmail.com>
2026-08-23 09:01:08 +0900

    feat(frontier): register gameplay content

--- projects/frontier-settlement/src/main/java/kr/moonseungjun/frontiersettlement/FrontierSettlement.java
+++ projects/frontier-settlement/src/main/java/kr/moonseungjun/frontiersettlement/FrontierSettlement.java
@@ -1,6 +1,7 @@
 package kr.moonseungjun.frontiersettlement;
 
 import kr.moonseungjun.frontiersettlement.command.SettlementCommands;
+import kr.moonseungjun.frontiersettlement.content.FrontierContent;
 import kr.moonseungjun.frontiersettlement.network.SettlementNetwork;
 import kr.moonseungjun.frontiersettlement.settlement.SettlementService;
 import net.neoforged.bus.api.IEventBus;
@@ -13,6 +14,7 @@ public final class FrontierSettlement {
     public static final String MOD_ID = "frontier_settlement";
 
     public FrontierSettlement(IEventBus modBus, ModContainer modContainer) {
+        FrontierContent.register(modBus);
         modBus.addListener(SettlementNetwork::onRegisterPayloads);
         NeoForge.EVENT_BUS.addListener(SettlementCommands::onRegisterCommands);
         NeoForge.EVENT_BUS.addListener(SettlementService::onServerTick);
