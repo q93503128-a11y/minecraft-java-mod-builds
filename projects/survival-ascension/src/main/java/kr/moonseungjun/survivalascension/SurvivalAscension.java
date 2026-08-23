27b117383666d04ae2e9d84ee31ff171a777f7b4
coworkaccount <cyp1jhh2@gmail.com>
2026-08-23 16:02:58 +0900

    Set Survival Ascension 0.28 runtime version [skip ci]

--- projects/survival-ascension/src/main/java/kr/moonseungjun/survivalascension/SurvivalAscension.java
+++ projects/survival-ascension/src/main/java/kr/moonseungjun/survivalascension/SurvivalAscension.java
@@ -28,7 +28,7 @@ import org.slf4j.Logger;
 @Mod(SurvivalAscension.MOD_ID)
 public final class SurvivalAscension {
     public static final String MOD_ID = "survivalascension";
-    public static final String VERSION = "0.27.0-alpha.1";
+    public static final String VERSION = "0.28.0-alpha.1";
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public SurvivalAscension(IEventBus modEventBus) {
@@ -73,6 +73,6 @@ public final class SurvivalAscension {
         NeoForge.EVENT_BUS.addListener(WorldAscensionProgression::onLivingDeath);
         NeoForge.EVENT_BUS.addListener(AscensionAffixes::onEliteDeath);
         NeoForge.EVENT_BUS.addListener(AscensionCommands::onRegisterCommands);
-        LOGGER.info("Survival Ascension {} loaded: expeditions + regional incidents + bounded behavior-driven apex hunts + Lv.100 Field Mastery", VERSION);
+        LOGGER.info("Survival Ascension {} loaded: scaled mastery + expeditions + apex hunts + four-line industrial production", VERSION);
     }
 }
