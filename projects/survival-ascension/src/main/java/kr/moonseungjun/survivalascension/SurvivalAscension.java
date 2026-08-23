219697d7be86dfc9670ac4567ab9b00065759a50
coworkaccount <cyp1jhh2@gmail.com>
2026-08-23 13:12:26 +0900

    Advance Survival Ascension to 0.25 directives [skip ci]

--- projects/survival-ascension/src/main/java/kr/moonseungjun/survivalascension/SurvivalAscension.java
+++ projects/survival-ascension/src/main/java/kr/moonseungjun/survivalascension/SurvivalAscension.java
@@ -26,7 +26,7 @@ import org.slf4j.Logger;
 @Mod(SurvivalAscension.MOD_ID)
 public final class SurvivalAscension {
     public static final String MOD_ID = "survivalascension";
-    public static final String VERSION = "0.24.0-alpha.1";
+    public static final String VERSION = "0.25.0-alpha.1";
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public SurvivalAscension(IEventBus modEventBus) {
@@ -66,6 +66,6 @@ public final class SurvivalAscension {
         NeoForge.EVENT_BUS.addListener(WorldAscensionProgression::onLivingDeath);
         NeoForge.EVENT_BUS.addListener(AscensionAffixes::onEliteDeath);
         NeoForge.EVENT_BUS.addListener(AscensionCommands::onRegisterCommands);
-        LOGGER.info("Survival Ascension {} loaded: validated nine-region field objectives + legacy-safe expedition progression + Lv.100 field mastery", VERSION);
+        LOGGER.info("Survival Ascension {} loaded: randomized multi-task regional directives + legacy-safe expeditions + Lv.100 field mastery", VERSION);
     }
 }
