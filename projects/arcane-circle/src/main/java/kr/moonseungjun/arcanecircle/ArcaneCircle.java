cbf3b471eb908a1278bb7ab9d9927fa9e7b056a5
coworkaccount <cyp1jhh2@gmail.com>
2026-08-23 09:48:21 +0900

    chore(arcane-circle): set runtime version alpha.66

--- projects/arcane-circle/src/main/java/kr/moonseungjun/arcanecircle/ArcaneCircle.java
+++ projects/arcane-circle/src/main/java/kr/moonseungjun/arcanecircle/ArcaneCircle.java
@@ -50,7 +50,7 @@ import org.slf4j.Logger;
 @Mod(ArcaneCircle.MOD_ID)
 public final class ArcaneCircle {
     public static final String MOD_ID = "arcanecircle";
-    public static final String VERSION = "0.12.1-alpha.65";
+    public static final String VERSION = "0.12.1-alpha.66";
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public ArcaneCircle(IEventBus modEventBus) {
