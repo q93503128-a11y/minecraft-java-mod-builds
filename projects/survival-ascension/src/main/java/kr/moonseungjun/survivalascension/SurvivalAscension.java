d3717a7113bacc55b43797046b2a54270a8df9c5
coworkaccount <cyp1jhh2@gmail.com>
2026-08-23 12:57:01 +0900

    Advance Survival Ascension to 0.24 field objectives [skip ci]

--- projects/survival-ascension/src/main/java/kr/moonseungjun/survivalascension/SurvivalAscension.java
+++ projects/survival-ascension/src/main/java/kr/moonseungjun/survivalascension/SurvivalAscension.java
@@ -26,7 +26,7 @@ import org.slf4j.Logger;
 @Mod(SurvivalAscension.MOD_ID)
 public final class SurvivalAscension {
     public static final String MOD_ID = "survivalascension";
-    public static final String VERSION = "0.23.0-alpha.1";
+    public static final String VERSION = "0.24.0-alpha.1";
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public SurvivalAscension(IEventBus modEventBus) {
@@ -50,6 +50,7 @@ public final class SurvivalAscension {
         NeoForge.EVENT_BUS.addListener(MobilityProgression::onPlayerLoggedOut);
         NeoForge.EVENT_BUS.addListener(ExpeditionProgression::onPlayerTick);
         NeoForge.EVENT_BUS.addListener(ExpeditionProgression::onPlayerLoggedIn);
+        NeoForge.EVENT_BUS.addListener(ExpeditionProgression::onPlayerLoggedOut);
         NeoForge.EVENT_BUS.addListener(EliteMobSystem::onFinalizeSpawn);
         NeoForge.EVENT_BUS.addListener(EliteMobSystem::onDamagePre);
         NeoForge.EVENT_BUS.addListener(EliteMobSystem::onDamagePost);
@@ -65,6 +66,6 @@ public final class SurvivalAscension {
         NeoForge.EVENT_BUS.addListener(WorldAscensionProgression::onLivingDeath);
         NeoForge.EVENT_BUS.addListener(AscensionAffixes::onEliteDeath);
         NeoForge.EVENT_BUS.addListener(AscensionCommands::onRegisterCommands);
-        LOGGER.info("Survival Ascension {} loaded: mastery VI + tactical warbands + doctrine trials + awakened mythic + persistent nine-region expeditions + Lv.100 field mastery", VERSION);
+        LOGGER.info("Survival Ascension {} loaded: mastery VI + tactical warbands + doctrine trials + awakened mythic + nine-region field objectives + Lv.100 field mastery", VERSION);
     }
 }
