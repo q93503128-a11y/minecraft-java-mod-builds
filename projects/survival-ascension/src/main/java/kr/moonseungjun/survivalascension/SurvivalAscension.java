489657f39f79c55f0b079c53a2715c65edead90f
coworkaccount <cyp1jhh2@gmail.com>
2026-08-23 09:15:01 +0900

    Survival Ascension 0.21 Ascension Trial

--- projects/survival-ascension/src/main/java/kr/moonseungjun/survivalascension/SurvivalAscension.java
+++ projects/survival-ascension/src/main/java/kr/moonseungjun/survivalascension/SurvivalAscension.java
@@ -7,6 +7,7 @@ import kr.moonseungjun.survivalascension.construction.ConstructionProgression;
 import kr.moonseungjun.survivalascension.elite.EliteMobSystem;
 import kr.moonseungjun.survivalascension.elite.EndgameMutationSystem;
 import kr.moonseungjun.survivalascension.elite.WarbandDirector;
+import kr.moonseungjun.survivalascension.endgame.AscensionTrialSystem;
 import kr.moonseungjun.survivalascension.equipment.AscensionAffixes;
 import kr.moonseungjun.survivalascension.harvesting.HarvestingProgression;
 import kr.moonseungjun.survivalascension.harvesting.IrrigationReplantService;
@@ -24,7 +25,7 @@ import org.slf4j.Logger;
 @Mod(SurvivalAscension.MOD_ID)
 public final class SurvivalAscension {
     public static final String MOD_ID = "survivalascension";
-    public static final String VERSION = "0.20.0-alpha.1";
+    public static final String VERSION = "0.21.0-alpha.1";
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public SurvivalAscension(IEventBus modEventBus) {
@@ -55,9 +56,10 @@ public final class SurvivalAscension {
         NeoForge.EVENT_BUS.addListener(WarbandDirector::onFinalizeSpawn);
         NeoForge.EVENT_BUS.addListener(WarbandDirector::onServerTick);
         NeoForge.EVENT_BUS.addListener(WarbandDirector::onLivingDeath);
+        NeoForge.EVENT_BUS.addListener(AscensionTrialSystem::onServerTick);
         NeoForge.EVENT_BUS.addListener(WorldAscensionProgression::onLivingDeath);
         NeoForge.EVENT_BUS.addListener(AscensionAffixes::onEliteDeath);
         NeoForge.EVENT_BUS.addListener(AscensionCommands::onRegisterCommands);
-        LOGGER.info("Survival Ascension {} loaded: six skills + mastery VI + tactical warbands + world ascension + endgame mutations", VERSION);
+        LOGGER.info("Survival Ascension {} loaded: six skills + mastery VI + tactical warbands + world ascension + endgame mutations + ascension trials", VERSION);
     }
 }
