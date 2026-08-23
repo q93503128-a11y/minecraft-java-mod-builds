fd0b995332d2088e8998b86ade79ea828afd242b
coworkaccount <cyp1jhh2@gmail.com>
2026-08-23 20:17:50 +0900

    feat(frontier): protect cart station freight infrastructure

--- projects/frontier-settlement/src/main/java/kr/moonseungjun/frontiersettlement/FrontierSettlement.java
+++ projects/frontier-settlement/src/main/java/kr/moonseungjun/frontiersettlement/FrontierSettlement.java
@@ -3,6 +3,7 @@ package kr.moonseungjun.frontiersettlement;
 import kr.moonseungjun.frontiersettlement.command.SettlementCommands;
 import kr.moonseungjun.frontiersettlement.content.FrontierContent;
 import kr.moonseungjun.frontiersettlement.network.SettlementNetwork;
+import kr.moonseungjun.frontiersettlement.settlement.SettlementCartStationService;
 import kr.moonseungjun.frontiersettlement.settlement.SettlementConstructionService;
 import kr.moonseungjun.frontiersettlement.settlement.SettlementCoreService;
 import kr.moonseungjun.frontiersettlement.settlement.SettlementMarketService;
@@ -33,5 +34,6 @@ public final class FrontierSettlement {
         NeoForge.EVENT_BUS.addListener(SettlementTierInfrastructureService::onBreakBlock);
         NeoForge.EVENT_BUS.addListener(SettlementMarketService::onBreakBlock);
         NeoForge.EVENT_BUS.addListener(SettlementWorkshopService::onBreakBlock);
+        NeoForge.EVENT_BUS.addListener(SettlementCartStationService::onBreakBlock);
     }
 }
