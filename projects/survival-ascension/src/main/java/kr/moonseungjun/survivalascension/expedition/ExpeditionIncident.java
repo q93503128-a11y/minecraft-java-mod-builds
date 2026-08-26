853cebf472bc65fce48f247429c0368f236086c3
coworkaccount <cyp1jhh2@gmail.com>
2026-08-26 12:44:49 +0900

    Double integrated-biome incident variety

--- projects/survival-ascension/src/main/java/kr/moonseungjun/survivalascension/expedition/ExpeditionIncident.java
+++ projects/survival-ascension/src/main/java/kr/moonseungjun/survivalascension/expedition/ExpeditionIncident.java
@@ -14,40 +14,54 @@ public enum ExpeditionIncident {
     WOODLAND_RUSH(ExpeditionRegion.WOODLAND, "벌목 비상", 900, ExpeditionAction.LOGS_FELLED, 24),
     WOODLAND_FRONTIER_AMBUSH(ExpeditionRegion.WOODLAND, "밀림 포위", 1200, 6,
             "minecraft:witch", "minecraft:spider", "minecraft:zombie"),
+    WOODLAND_FRONTIER_RUSH(ExpeditionRegion.WOODLAND, "고목길 개척", 900, ExpeditionAction.LOGS_FELLED, 32),
 
     ARID_AMBUSH(ExpeditionRegion.ARID, "약탈대 급습", 1200, 6, "minecraft:husk", "minecraft:pillager"),
     ARID_RUSH(ExpeditionRegion.ARID, "긴급 보급선", 900, ExpeditionAction.BLOCKS_BUILT, 24),
     ARID_FRONTIER_RUSH(ExpeditionRegion.ARID, "황무지 방벽 구축", 900, ExpeditionAction.BLOCKS_BUILT, 30),
+    ARID_FRONTIER_AMBUSH(ExpeditionRegion.ARID, "사막 협곡 포위", 1200, 6,
+            "minecraft:husk", "minecraft:pillager", "minecraft:spider"),
 
     WETLAND_AMBUSH(ExpeditionRegion.WETLAND, "늪지 습격", 1200, 6, "minecraft:zombie", "minecraft:spider"),
     WETLAND_RUSH(ExpeditionRegion.WETLAND, "긴급 수확", 900, ExpeditionAction.CROPS_HARVESTED, 20),
     WETLAND_FRONTIER_AMBUSH(ExpeditionRegion.WETLAND, "습원 마녀습격", 1200, 6,
             "minecraft:witch", "minecraft:slime", "minecraft:spider"),
+    WETLAND_FRONTIER_RUSH(ExpeditionRegion.WETLAND, "침수지 긴급 수확", 900, ExpeditionAction.CROPS_HARVESTED, 28),
 
     HIGHLANDS_AMBUSH(ExpeditionRegion.HIGHLANDS, "능선 매복", 1200, 6, "minecraft:pillager", "minecraft:skeleton"),
     HIGHLANDS_RUSH(ExpeditionRegion.HIGHLANDS, "능선 돌파", 900, ExpeditionAction.DASHES_USED, 4),
     HIGHLANDS_FRONTIER_RUSH(ExpeditionRegion.HIGHLANDS, "절벽 강행 돌파", 900, ExpeditionAction.DASHES_USED, 5),
+    HIGHLANDS_FRONTIER_AMBUSH(ExpeditionRegion.HIGHLANDS, "절벽 사냥대", 1200, 6,
+            "minecraft:pillager", "minecraft:skeleton", "minecraft:spider"),
 
     OCEAN_AMBUSH(ExpeditionRegion.OCEAN, "익사자 습격", 1200, 6, "minecraft:drowned"),
     OCEAN_RUSH(ExpeditionRegion.OCEAN, "폭풍 항해", 900, ExpeditionAction.OCEAN_VOYAGE, 180),
+    OCEAN_FRONTIER_AMBUSH(ExpeditionRegion.OCEAN, "난류 익사자 떼", 1200, 7, "minecraft:drowned"),
+    OCEAN_FRONTIER_RUSH(ExpeditionRegion.OCEAN, "외해 돌파", 900, ExpeditionAction.OCEAN_VOYAGE, 240),
 
     DEEP_AMBUSH(ExpeditionRegion.DEEP, "심층 군집", 1200, 7, "minecraft:zombie", "minecraft:skeleton", "minecraft:spider"),
     DEEP_RUSH(ExpeditionRegion.DEEP, "붕괴 전 채굴", 900, ExpeditionAction.BLOCKS_MINED, 48),
     DEEP_FRONTIER_AMBUSH(ExpeditionRegion.DEEP, "동굴 군집 붕괴", 1200, 7,
             "minecraft:cave_spider", "minecraft:silverfish", "minecraft:zombie"),
+    DEEP_FRONTIER_RUSH(ExpeditionRegion.DEEP, "공명 광맥 긴급 굴착", 900, ExpeditionAction.BLOCKS_MINED, 64),
 
     FROZEN_AMBUSH(ExpeditionRegion.FROZEN, "설원 습격", 1200, 6, "minecraft:stray", "minecraft:skeleton"),
     FROZEN_RUSH(ExpeditionRegion.FROZEN, "빙설 강행군", 900, ExpeditionAction.TRAVEL_DISTANCE, 180),
     FROZEN_FRONTIER_RUSH(ExpeditionRegion.FROZEN, "눈보라 강행군", 900, ExpeditionAction.TRAVEL_DISTANCE, 220),
+    FROZEN_FRONTIER_AMBUSH(ExpeditionRegion.FROZEN, "설맹 포위", 1200, 6,
+            "minecraft:stray", "minecraft:skeleton", "minecraft:spider"),
 
     NETHER_AMBUSH(ExpeditionRegion.NETHER, "네더 급습", 1200, 8, "minecraft:wither_skeleton", "minecraft:blaze"),
     NETHER_RUSH(ExpeditionRegion.NETHER, "열기 속 채굴", 900, ExpeditionAction.BLOCKS_MINED, 48),
     NETHER_FRONTIER_AMBUSH(ExpeditionRegion.NETHER, "용암 협곡 습격", 1200, 8,
             "minecraft:magma_cube", "minecraft:blaze", "minecraft:wither_skeleton"),
+    NETHER_FRONTIER_RUSH(ExpeditionRegion.NETHER, "화산맥 긴급 굴착", 900, ExpeditionAction.BLOCKS_MINED, 64),
 
     END_AMBUSH(ExpeditionRegion.END, "공허 습격", 1200, 7, "minecraft:endermite", "minecraft:shulker"),
     END_RUSH(ExpeditionRegion.END, "공허 추적", 900, ExpeditionAction.TRAVEL_DISTANCE, 180),
-    END_FRONTIER_RUSH(ExpeditionRegion.END, "공허 균열 사냥", 900, ExpeditionAction.HOSTILES_KILLED, 10);
+    END_FRONTIER_RUSH(ExpeditionRegion.END, "공허 균열 사냥", 900, ExpeditionAction.HOSTILES_KILLED, 10),
+    END_FRONTIER_AMBUSH(ExpeditionRegion.END, "균열 포위", 1200, 7,
+            "minecraft:endermite", "minecraft:shulker");
 
     public enum Kind { AMBUSH, ACTION_RUSH }
 
@@ -98,7 +112,7 @@ public enum ExpeditionIncident {
     }
 
     /**
-     * Data-integrated terrain receives one additional regional incident instead of merely sharing
+     * Data-integrated terrain receives two additional regional incidents instead of merely sharing
      * the vanilla fallback pair. External biome IDs remain in data tags rather than Java.
      */
     public static List<ExpeditionIncident> forRegion(ExpeditionRegion region, boolean integrationTagged) {
@@ -116,15 +130,15 @@ public enum ExpeditionIncident {
             };
         }
         return switch (region) {
-            case WOODLAND -> List.of(WOODLAND_AMBUSH, WOODLAND_RUSH, WOODLAND_FRONTIER_AMBUSH);
-            case ARID -> List.of(ARID_AMBUSH, ARID_RUSH, ARID_FRONTIER_RUSH);
-            case WETLAND -> List.of(WETLAND_AMBUSH, WETLAND_RUSH, WETLAND_FRONTIER_AMBUSH);
-            case HIGHLANDS -> List.of(HIGHLANDS_AMBUSH, HIGHLANDS_RUSH, HIGHLANDS_FRONTIER_RUSH);
-            case OCEAN -> List.of(OCEAN_AMBUSH, OCEAN_RUSH);
-            case DEEP -> List.of(DEEP_AMBUSH, DEEP_RUSH, DEEP_FRONTIER_AMBUSH);
-            case FROZEN -> List.of(FROZEN_AMBUSH, FROZEN_RUSH, FROZEN_FRONTIER_RUSH);
-            case NETHER -> List.of(NETHER_AMBUSH, NETHER_RUSH, NETHER_FRONTIER_AMBUSH);
-            case END -> List.of(END_AMBUSH, END_RUSH, END_FRONTIER_RUSH);
+            case WOODLAND -> List.of(WOODLAND_AMBUSH, WOODLAND_RUSH, WOODLAND_FRONTIER_AMBUSH, WOODLAND_FRONTIER_RUSH);
+            case ARID -> List.of(ARID_AMBUSH, ARID_RUSH, ARID_FRONTIER_RUSH, ARID_FRONTIER_AMBUSH);
+            case WETLAND -> List.of(WETLAND_AMBUSH, WETLAND_RUSH, WETLAND_FRONTIER_AMBUSH, WETLAND_FRONTIER_RUSH);
+            case HIGHLANDS -> List.of(HIGHLANDS_AMBUSH, HIGHLANDS_RUSH, HIGHLANDS_FRONTIER_RUSH, HIGHLANDS_FRONTIER_AMBUSH);
+            case OCEAN -> List.of(OCEAN_AMBUSH, OCEAN_RUSH, OCEAN_FRONTIER_AMBUSH, OCEAN_FRONTIER_RUSH);
+            case DEEP -> List.of(DEEP_AMBUSH, DEEP_RUSH, DEEP_FRONTIER_AMBUSH, DEEP_FRONTIER_RUSH);
+            case FROZEN -> List.of(FROZEN_AMBUSH, FROZEN_RUSH, FROZEN_FRONTIER_RUSH, FROZEN_FRONTIER_AMBUSH);
+            case NETHER -> List.of(NETHER_AMBUSH, NETHER_RUSH, NETHER_FRONTIER_AMBUSH, NETHER_FRONTIER_RUSH);
+            case END -> List.of(END_AMBUSH, END_RUSH, END_FRONTIER_RUSH, END_FRONTIER_AMBUSH);
         };
     }
 
