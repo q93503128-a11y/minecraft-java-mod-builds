3611037911f0bc5d8ec58218cb0964431fa62d41
coworkaccount <cyp1jhh2@gmail.com>
2026-08-27 10:49:43 +0900

    Register TITANBREAK Reflex Drive

---
+++ projects/titanbreak/src/main/java/kr/moonseungjun/titanbreak/registry/ModItems.java
@@ -0,0 +1,29 @@
+package kr.moonseungjun.titanbreak.registry;
+
+import kr.moonseungjun.titanbreak.Titanbreak;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public final class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Titanbreak.MOD_ID);
+
+    public static final DeferredItem<Item> REFLEX_DRIVE_I = ITEMS.registerSimpleItem(
+            "reflex_drive_i",
+            properties -> properties.stacksTo(1).rarity(Rarity.RARE));
+
+    private ModItems() {}
+
+    public static void register(IEventBus bus) {
+        ITEMS.register(bus);
+        bus.addListener(ModItems::addCreativeItems);
+    }
+
+    private static void addCreativeItems(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.COMBAT) event.accept(REFLEX_DRIVE_I.get());
+    }
+}
