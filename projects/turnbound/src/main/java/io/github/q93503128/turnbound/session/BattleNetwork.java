48e8a18d905c93a0b3a1ab1c9c9910ccc2f88492
coworkaccount <cyp1jhh2@gmail.com>
2026-08-29 11:19:40 +0900

    Bump TURNBOUND battle network protocol for alpha.5

--- projects/turnbound/src/main/java/io/github/q93503128/turnbound/session/BattleNetwork.java
+++ projects/turnbound/src/main/java/io/github/q93503128/turnbound/session/BattleNetwork.java
@@ -8,7 +8,7 @@ import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 import net.neoforged.neoforge.network.registration.PayloadRegistrar;
 
 public final class BattleNetwork {
-    public static final String PROTOCOL = "turnbound-alpha2";
+    public static final String PROTOCOL = "turnbound-alpha5";
 
     private BattleNetwork() {}
 
