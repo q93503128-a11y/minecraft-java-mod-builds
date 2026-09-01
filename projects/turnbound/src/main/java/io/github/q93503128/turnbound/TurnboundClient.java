1fc91700a0ed9087e4c253e34d3498fb10a75bd0
coworkaccount <cyp1jhh2@gmail.com>
2026-09-01 09:20:04 +0900

    turnbound: register client audio cue handler

--- projects/turnbound/src/main/java/io/github/q93503128/turnbound/TurnboundClient.java
+++ projects/turnbound/src/main/java/io/github/q93503128/turnbound/TurnboundClient.java
@@ -1,6 +1,7 @@
 package io.github.q93503128.turnbound;
 
 import io.github.q93503128.turnbound.client.BattleCameraController;
+import io.github.q93503128.turnbound.client.ClientAudioNetwork;
 import io.github.q93503128.turnbound.client.ClientBattleNetwork;
 import io.github.q93503128.turnbound.client.ClientFieldNetwork;
 import io.github.q93503128.turnbound.client.ClientMetaNetwork;
@@ -17,6 +18,7 @@ public final class TurnboundClient {
         modEventBus.addListener(ClientBattleNetwork::register);
         modEventBus.addListener(ClientFieldNetwork::register);
         modEventBus.addListener(ClientMetaNetwork::register);
+        modEventBus.addListener(ClientAudioNetwork::register);
         NeoForge.EVENT_BUS.addListener(VanillaHudPolicy::onGuiLayer);
         NeoForge.EVENT_BUS.addListener(BattleCameraController::onDetachedCameraDistance);
         NeoForge.EVENT_BUS.addListener(BattleCameraController::onCameraAngles);
