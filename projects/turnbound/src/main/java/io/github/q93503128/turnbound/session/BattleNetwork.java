cf865eaab43018c76f7b39205ad4ca7ddaca0180
coworkaccount <cyp1jhh2@gmail.com>
2026-09-01 10:55:50 +0900

    turnbound: send player-aware result preview snapshots

--- projects/turnbound/src/main/java/io/github/q93503128/turnbound/session/BattleNetwork.java
+++ projects/turnbound/src/main/java/io/github/q93503128/turnbound/session/BattleNetwork.java
@@ -24,7 +24,7 @@ public final class BattleNetwork {
     }
 
     static void sync(ServerPlayer player, BattleSession session) {
-        PacketDistributor.sendToPlayer(player, new BattleSnapshotPayload(BattleSnapshotCodec.encode(session)));
+        PacketDistributor.sendToPlayer(player, new BattleSnapshotPayload(BattleSnapshotCodec.encode(player.getUUID(), session)));
     }
 
     static void close(ServerPlayer player) {
