1abcbe1c103268ef647897f4599d1bfc28b2ee22
coworkaccount <cyp1jhh2@gmail.com>
2026-09-01 13:54:40 +0900

    Gate temporary construction matrix QA in NeoForge runtime

--- projects/frontier-settlement/src/main/java/kr/moonseungjun/frontiersettlement/FrontierSettlement.java
+++ projects/frontier-settlement/src/main/java/kr/moonseungjun/frontiersettlement/FrontierSettlement.java
@@ -3,6 +3,7 @@ package kr.moonseungjun.frontiersettlement;
 import kr.moonseungjun.frontiersettlement.command.SettlementCommands;
 import kr.moonseungjun.frontiersettlement.content.FrontierContent;
 import kr.moonseungjun.frontiersettlement.network.SettlementNetwork;
+import kr.moonseungjun.frontiersettlement.settlement.ConstructionMatrixQa;
 import kr.moonseungjun.frontiersettlement.settlement.SettlementAdvancedWorkshopService;
 import kr.moonseungjun.frontiersettlement.settlement.SettlementBarracksService;
 import kr.moonseungjun.frontiersettlement.settlement.SettlementBenefitService;
@@ -55,5 +56,8 @@ public final class FrontierSettlement {
         NeoForge.EVENT_BUS.addListener(SettlementMilitaryOutpostService::onLivingDrops);
         NeoForge.EVENT_BUS.addListener(SettlementOutpostLogisticsService::onLivingDrops);
         NeoForge.EVENT_BUS.addListener(SettlementWorkerService::onLivingDrops);
+        if ("1".equals(System.getenv("FRONTIER_MATRIX_QA"))) {
+            NeoForge.EVENT_BUS.addListener(ConstructionMatrixQa::onServerStarted);
+        }
     }
 }
