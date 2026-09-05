7052444c1e4784050705dbbd1173e8916c2b96ec
coworkaccount <cyp1jhh2@gmail.com>
2026-09-05 22:08:01 +0900

    fix(turnbound): protect battle actors from Radia cleanup

--- projects/turnbound/src/main/java/io/github/q93503128/turnbound/world/RadiaHubSessionManager.java
+++ projects/turnbound/src/main/java/io/github/q93503128/turnbound/world/RadiaHubSessionManager.java
@@ -3,6 +3,7 @@ package io.github.q93503128.turnbound.world;
 import io.github.q93503128.turnbound.combat.BattleOutcome;
 import io.github.q93503128.turnbound.combat.CampaignEncounterCatalog;
 import io.github.q93503128.turnbound.content.V04Catalogs;
+import io.github.q93503128.turnbound.presentation.BattleActorEntity;
 import io.github.q93503128.turnbound.session.BattleSessionManager;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
@@ -176,7 +177,9 @@ public final class RadiaHubSessionManager {
 
     private static void clearMobs(ServerLevel level) {
         AABB area = new AABB(-132, 54, -116, 132, 100, 132);
-        for (Mob mob : level.getEntitiesOfClass(Mob.class, area)) mob.discard();
+        for (Mob mob : level.getEntitiesOfClass(Mob.class, area)) {
+            if (!(mob instanceof BattleActorEntity)) mob.discard();
+        }
     }
 
     private static final class Session {
