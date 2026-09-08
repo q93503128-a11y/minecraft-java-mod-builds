b1b7cad451f6dc98f3359b15f958a00ea9e48143
coworkaccount <cyp1jhh2@gmail.com>
2026-09-08 14:29:30 +0900

    feat(earth-to-stars): wire M1 survival content registry [skip ci]

--- projects/earth-to-stars/src/main/java/kr/moonseungjun/earthtostars/EarthToStars.java
+++ projects/earth-to-stars/src/main/java/kr/moonseungjun/earthtostars/EarthToStars.java
@@ -1,6 +1,7 @@
 package kr.moonseungjun.earthtostars;
 
 import com.mojang.logging.LogUtils;
+import kr.moonseungjun.earthtostars.content.EarthToStarsItems;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import org.slf4j.Logger;
@@ -8,10 +9,11 @@ import org.slf4j.Logger;
 @Mod(EarthToStars.MOD_ID)
 public final class EarthToStars {
     public static final String MOD_ID = "earth_to_stars";
-    public static final String VERSION = "0.1.0-alpha.8";
+    public static final String VERSION = "0.1.0-alpha.9";
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public EarthToStars(IEventBus modEventBus) {
-        LOGGER.info("EARTH TO STARS {} progression independence guard loaded", VERSION);
+        EarthToStarsItems.register(modEventBus);
+        LOGGER.info("EARTH TO STARS {} Earth launch progression loaded", VERSION);
     }
 }
