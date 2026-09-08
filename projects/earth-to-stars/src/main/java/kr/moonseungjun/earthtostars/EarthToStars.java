22ff3b0058a9edf3e809585d12541062797db9ed
coworkaccount <cyp1jhh2@gmail.com>
2026-09-08 13:57:25 +0900

    chore(earth-to-stars): advance lifecycle version marker [skip ci]

--- projects/earth-to-stars/src/main/java/kr/moonseungjun/earthtostars/EarthToStars.java
+++ projects/earth-to-stars/src/main/java/kr/moonseungjun/earthtostars/EarthToStars.java
@@ -8,10 +8,10 @@ import org.slf4j.Logger;
 @Mod(EarthToStars.MOD_ID)
 public final class EarthToStars {
     public static final String MOD_ID = "earth_to_stars";
-    public static final String VERSION = "0.1.0-alpha.6";
+    public static final String VERSION = "0.1.0-alpha.7";
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public EarthToStars(IEventBus modEventBus) {
-        LOGGER.info("EARTH TO STARS {} central ship systems backend loaded", VERSION);
+        LOGGER.info("EARTH TO STARS {} lifecycle persistence backend loaded", VERSION);
     }
 }
