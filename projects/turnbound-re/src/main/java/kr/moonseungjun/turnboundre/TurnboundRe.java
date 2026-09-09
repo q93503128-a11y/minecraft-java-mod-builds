3188bdca03a93e36222afae237312b91af3b9846
coworkaccount <cyp1jhh2@gmail.com>
2026-09-09 18:11:40 +0900

    turnbound-re: retire physical expedition journal access

--- projects/turnbound-re/src/main/java/kr/moonseungjun/turnboundre/TurnboundRe.java
+++ projects/turnbound-re/src/main/java/kr/moonseungjun/turnboundre/TurnboundRe.java
@@ -13,7 +13,6 @@ import kr.moonseungjun.turnboundre.network.BattleResultPresentationService;
 import kr.moonseungjun.turnboundre.progression.BattleRewardLifecycleHooks;
 import kr.moonseungjun.turnboundre.progression.BattleRewardSettlementService;
 import kr.moonseungjun.turnboundre.progression.PlayerProgressStore;
-import kr.moonseungjun.turnboundre.world.ExpeditionJournalLifecycle;
 import kr.moonseungjun.turnboundre.world.VanillaMobWorldPolicy;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
@@ -40,7 +39,6 @@ public final class TurnboundRe {
         NeoForge.EVENT_BUS.addListener(this::addServerReloadListeners);
         new BattleWorldEventHooks(BATTLES).register(NeoForge.EVENT_BUS);
         new BattleRewardLifecycleHooks(BATTLES, REWARD_SETTLEMENT, RESULT_PRESENTATION).register(NeoForge.EVENT_BUS);
-        new ExpeditionJournalLifecycle().register(NeoForge.EVENT_BUS);
         new VanillaMobWorldPolicy().register(NeoForge.EVENT_BUS);
         LOGGER.info("TURNBOUND: RE {} natural expedition/battle lifecycle loaded", VERSION);
     }
