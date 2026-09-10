14f9daba32fedaf555d037442fc1e3653a04d29d
coworkaccount <cyp1jhh2@gmail.com>
2026-09-10 09:47:20 +0900

    turnbound-re: retire legacy management item hooks

--- projects/turnbound-re/src/main/java/kr/moonseungjun/turnboundre/client/input/ExpeditionJournalInput.java
+++
@@ -1,9 +0,0 @@
-package kr.moonseungjun.turnboundre.client.input;
-
-/**
- * Legacy marker kept only so old development worlds can still contain the former journal item safely.
- * TURNBOUND system screens are now entered through the unified M menu instead of item-use interception.
- */
-public final class ExpeditionJournalInput {
-    private ExpeditionJournalInput() {}
-}
--- projects/turnbound-re/src/main/java/kr/moonseungjun/turnboundre/client/input/PartyInputHandler.java
+++
@@ -1,6 +0,0 @@
-package kr.moonseungjun.turnboundre.client.input;
-
-/** Party Formation is opened from the Expedition Journal. No standalone TURNBOUND key mapping is registered. */
-public final class PartyInputHandler {
-    private PartyInputHandler() {}
-}
