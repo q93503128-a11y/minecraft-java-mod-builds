591eb9ff03550cdb30eeca6566333ab56874c6fe
coworkaccount <cyp1jhh2@gmail.com>
2026-09-13 12:13:46 +0900

    fishing-game: expose tall and angler model layers

--- projects/fishing-game/src/client/java/dev/moonseungjun/fishinggame/client/fish/EncounterFishModelLayers.java
+++ projects/fishing-game/src/client/java/dev/moonseungjun/fishinggame/client/fish/EncounterFishModelLayers.java
@@ -4,12 +4,11 @@ import dev.moonseungjun.fishinggame.FishingGameMod;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 
 public final class EncounterFishModelLayers {
-    public static final ModelLayerLocation SMALL =
-            new ModelLayerLocation(FishingGameMod.id("encounter_small_fish"), "main");
-    public static final ModelLayerLocation FAT =
-            new ModelLayerLocation(FishingGameMod.id("encounter_fat_fish"), "main");
-    public static final ModelLayerLocation LONG =
-            new ModelLayerLocation(FishingGameMod.id("encounter_long_fish"), "main");
+    public static final ModelLayerLocation SMALL = new ModelLayerLocation(FishingGameMod.id("encounter_small_fish"), "main");
+    public static final ModelLayerLocation TALL = new ModelLayerLocation(FishingGameMod.id("encounter_tall_fish"), "main");
+    public static final ModelLayerLocation FAT = new ModelLayerLocation(FishingGameMod.id("encounter_fat_fish"), "main");
+    public static final ModelLayerLocation LONG = new ModelLayerLocation(FishingGameMod.id("encounter_long_fish"), "main");
+    public static final ModelLayerLocation ANGLER = new ModelLayerLocation(FishingGameMod.id("encounter_angler_fish"), "main");
 
     private EncounterFishModelLayers() {
     }
