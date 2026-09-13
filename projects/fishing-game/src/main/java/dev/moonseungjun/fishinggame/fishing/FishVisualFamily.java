99b487b7d8f850ed4ac45b8649b7f530efe5c789
coworkaccount <cyp1jhh2@gmail.com>
2026-09-13 12:13:10 +0900

    fishing-game: expand encounter fish families

--- projects/fishing-game/src/main/java/dev/moonseungjun/fishinggame/fishing/FishVisualFamily.java
+++ projects/fishing-game/src/main/java/dev/moonseungjun/fishinggame/fishing/FishVisualFamily.java
@@ -2,13 +2,17 @@ package dev.moonseungjun.fishinggame.fishing;
 
 public enum FishVisualFamily {
     SMALL,
+    TALL,
     FAT,
-    LONG;
+    LONG,
+    ANGLER;
 
     public static FishVisualFamily forSpecies(String speciesId) {
         return switch (speciesId) {
-            case "bluegill", "mackerel" -> SMALL;
-            case "catfish", "oarfish", "ancient_sturgeon", "angler" -> LONG;
+            case "bluegill" -> TALL;
+            case "mackerel" -> SMALL;
+            case "catfish", "oarfish", "ancient_sturgeon" -> LONG;
+            case "angler" -> ANGLER;
             default -> FAT;
         };
     }
