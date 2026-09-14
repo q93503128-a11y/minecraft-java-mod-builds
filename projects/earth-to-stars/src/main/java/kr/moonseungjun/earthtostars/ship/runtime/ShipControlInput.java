b81876e8628c624566616f24d3d451b7ca52b09c
coworkaccount <cyp1jhh2@gmail.com>
2026-09-14 10:29:17 +0900

    earth-to-stars: begin alpha17 flight control rescue [skip ci]

--- projects/earth-to-stars/src/main/java/kr/moonseungjun/earthtostars/ship/runtime/ShipControlInput.java
+++ projects/earth-to-stars/src/main/java/kr/moonseungjun/earthtostars/ship/runtime/ShipControlInput.java
@@ -1,12 +1,12 @@
 package kr.moonseungjun.earthtostars.ship.runtime;
 
-public record ShipControlInput(double throttle, double yaw, double pitch) {
+public record ShipControlInput(double throttle, double yaw, double lift) {
     public static final ShipControlInput ZERO = new ShipControlInput(0.0D, 0.0D, 0.0D);
 
     public ShipControlInput {
         requireAxis(throttle, "throttle");
         requireAxis(yaw, "yaw");
-        requireAxis(pitch, "pitch");
+        requireAxis(lift, "lift");
     }
 
     private static void requireAxis(double value, String name) {
