79ae1abfcd28508837027aaa6e7c5cdff7dc17ed
coworkaccount <cyp1jhh2@gmail.com>
2026-09-14 10:22:16 +0900

    turnbound-re: wire natural authored world entry

--- projects/turnbound-re/src/main/java/kr/moonseungjun/turnboundre/TurnboundRe.java
+++ projects/turnbound-re/src/main/java/kr/moonseungjun/turnboundre/TurnboundRe.java
@@ -16,6 +16,7 @@ import kr.moonseungjun.turnboundre.progression.BattleRewardLifecycleHooks;
 import kr.moonseungjun.turnboundre.progression.BattleRewardSettlementService;
 import kr.moonseungjun.turnboundre.progression.EquipmentForgeService;
 import kr.moonseungjun.turnboundre.progression.PlayerProgressStore;
+import kr.moonseungjun.turnboundre.world.AuthoredWorldBootstrapService;
 import kr.moonseungjun.turnboundre.world.FirstExpeditionQuestService;
 import kr.moonseungjun.turnboundre.world.VanillaMobWorldPolicy;
 import kr.moonseungjun.turnboundre.world.WorldEncounterAnchorHooks;
@@ -39,6 +40,8 @@ public final class TurnboundRe {
     public static final EquipmentForgeService EQUIPMENT_FORGE = new EquipmentForgeService(DEFINITIONS, PROGRESS);
     public static final WorldFastTravelService FAST_TRAVEL = new WorldFastTravelService(DEFINITIONS, BATTLES);
     public static final FirstExpeditionQuestService FIRST_EXPEDITION = new FirstExpeditionQuestService(PROGRESS);
+    public static final AuthoredWorldBootstrapService WORLD_BOOTSTRAP =
+            new AuthoredWorldBootstrapService(DEFINITIONS, FIRST_EXPEDITION);
     public static final AuthoredEncounterLauncher AUTHORED_ENCOUNTERS = new AuthoredEncounterLauncher(BATTLES, DEFINITIONS);
     public static final BattleRewardSettlementService REWARD_SETTLEMENT = new BattleRewardSettlementService(BATTLES, PROGRESS);
     public static final BattleResultPresentationService RESULT_PRESENTATION = new BattleResultPresentationService(BATTLES);
@@ -47,6 +50,7 @@ public final class TurnboundRe {
         modEventBus.addListener(BattleNetwork::register);
         NeoForge.EVENT_BUS.addListener(this::registerCommands);
         NeoForge.EVENT_BUS.addListener(this::addServerReloadListeners);
+        WORLD_BOOTSTRAP.register(NeoForge.EVENT_BUS);
         new BattleWorldEventHooks(BATTLES).register(NeoForge.EVENT_BUS);
         new BattleRewardLifecycleHooks(BATTLES, REWARD_SETTLEMENT, RESULT_PRESENTATION, FIRST_EXPEDITION)
                 .register(NeoForge.EVENT_BUS);
