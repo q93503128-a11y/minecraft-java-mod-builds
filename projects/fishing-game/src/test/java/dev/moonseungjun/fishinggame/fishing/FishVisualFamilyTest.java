7ab204405b9bcab5c6675a55050b1f39e491d014
coworkaccount <cyp1jhh2@gmail.com>
2026-09-16 15:16:10 +0900

    fishing-game: align morphology contract tests

--- projects/fishing-game/src/test/java/dev/moonseungjun/fishinggame/fishing/FishVisualFamilyTest.java
+++ projects/fishing-game/src/test/java/dev/moonseungjun/fishinggame/fishing/FishVisualFamilyTest.java
@@ -8,20 +8,26 @@ class FishVisualFamilyTest {
     @Test
     void lakesideSpeciesUsePurposefulSilhouettes() {
         assertEquals(FishVisualFamily.TALL, FishVisualFamily.forSpecies("bluegill"));
-        assertEquals(FishVisualFamily.FAT, FishVisualFamily.forSpecies("carp"));
+        assertEquals(FishVisualFamily.CYPRINID, FishVisualFamily.forSpecies("crucian"));
+        assertEquals(FishVisualFamily.CYPRINID, FishVisualFamily.forSpecies("carp"));
+        assertEquals(FishVisualFamily.CYPRINID, FishVisualFamily.forSpecies("golden_carp"));
         assertEquals(FishVisualFamily.FAT, FishVisualFamily.forSpecies("largemouth"));
-        assertEquals(FishVisualFamily.LONG, FishVisualFamily.forSpecies("catfish"));
-        assertEquals(FishVisualFamily.ANGLER, FishVisualFamily.forSpecies("angler"));
+        assertEquals(FishVisualFamily.CATFISH, FishVisualFamily.forSpecies("catfish"));
     }
 
     @Test
-    void futureLargeLongFishDoNotFallBackToGenericFatShape() {
+    void streamlinedAndCompressedSpeciesDoNotFallBackToGenericFatShape() {
+        assertEquals(FishVisualFamily.PELAGIC, FishVisualFamily.forSpecies("trout"));
+        assertEquals(FishVisualFamily.PELAGIC, FishVisualFamily.forSpecies("salmon"));
+        assertEquals(FishVisualFamily.PELAGIC, FishVisualFamily.forSpecies("tuna"));
+        assertEquals(FishVisualFamily.BREAM, FishVisualFamily.forSpecies("sea_bream"));
+        assertEquals(FishVisualFamily.SMALL, FishVisualFamily.forSpecies("mackerel"));
+    }
+
+    @Test
+    void rareLongAndAnglerFishKeepSpecializedFamilies() {
+        assertEquals(FishVisualFamily.ANGLER, FishVisualFamily.forSpecies("angler"));
         assertEquals(FishVisualFamily.LONG, FishVisualFamily.forSpecies("oarfish"));
         assertEquals(FishVisualFamily.LONG, FishVisualFamily.forSpecies("ancient_sturgeon"));
-    }
-
-    @Test
-    void smallStreamlinedFishStayInSmallFamily() {
-        assertEquals(FishVisualFamily.SMALL, FishVisualFamily.forSpecies("mackerel"));
     }
 }
