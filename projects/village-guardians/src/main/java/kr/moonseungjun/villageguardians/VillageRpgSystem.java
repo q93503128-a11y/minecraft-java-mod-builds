e9ac80c385e029dd9344f39fc52cb98ba35a3630
coworkaccount <cyp1jhh2@gmail.com>
2026-09-21 18:08:11 +0900

    fix(village-guardians): import server level for rider damage redirect

--- projects/village-guardians/src/main/java/kr/moonseungjun/villageguardians/VillageRpgSystem.java
+++ projects/village-guardians/src/main/java/kr/moonseungjun/villageguardians/VillageRpgSystem.java
@@ -4,6 +4,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
